12deae5e3861f4da62b6c9bdfc897feb8adeb923
yuepengjiang <jiangdada12344321@163.com>
2019-05-21 21:28:42 +0800

    done

--- test.java
+++
@@ -1,6 +0,0 @@
-public class jyp{
-
-  public static void main(String[] args) {
-    
-  }
-}
--- test2.java
+++
@@ -1,6 +0,0 @@
-public class jyp{
-  public static void main(String[] args) {
-    System.out.println("jyp");
-    
-  }
-}
