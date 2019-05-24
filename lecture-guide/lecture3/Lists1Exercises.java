83044fa4ef1174454d462a809cc8e026ae91d6fa
yuepengjiang <jiangdada12344321@163.com>
2019-05-24 11:47:34 +0800

    complete lecture3

--- lecture-guide/lecture3/Lists1Exercises.java
+++ lecture-guide/lecture3/Lists1Exercises.java
@@ -3,17 +3,27 @@ public class Lists1Exercises {
       * each element incremented by x. L is not allowed
       * to change. */
     public static IntList incrList(IntList L, int x) {
-        for(int i = 0;i <= L.seze()-1;i++){
-
-        }
-        return L;
+      if(L.rest == null){
+        return new IntList(L.first + x,null);
+      }
+      else{
+        return new IntList(L.first+x,incrList(L.rest,x));
+      }
     }
 
     /** Returns an IntList identical to L, but with
       * each element incremented by x. Not allowed to use
       * the 'new' keyword. */
     public static IntList dincrList(IntList L, int x) {
-        /* Your code here. */
+        /* this means change L? */
+        if(L.rest == null){
+          L.first = L.first + x;
+          return L;
+        }
+        else{
+          L.first = L.first + x;
+          L.rest = dincrList(L.rest,x);
+        }
         return L;
     }
 
@@ -22,9 +32,16 @@ public class Lists1Exercises {
         L.rest = new IntList(7, null);
         L.rest.rest = new IntList(9, null);
 
-        System.out.println(L.size());
-        System.out.println(L.iterativeSize());
-
+        // System.out.println(L.size());
+        // System.out.println(L.iterativeSize());
+        IntList refer = incrList(L,100);
+        IntList refer2 = dincrList(L,100);
+        /* refer2 is just L*/
+        System.out.println(refer.first);
+        System.out.println(refer.rest.first);
+        System.out.println(refer.rest.rest.first);
+        System.out.println(refer2.first);
+        System.out.println(L.first);
         // Test your answers by uncommenting. Or copy and paste the
         // code for incrList and dincrList into IntList.java and
         // run it in the visualizer.
